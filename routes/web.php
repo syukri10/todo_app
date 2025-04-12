2661fd601fd8e12d9c61719335add56fb850c264
syukriothman10 <syukriothman10@gmail.com>
2025-04-12 16:15:34 +0800

    Updated root routes
    -use TodoController@index for root route

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
 
-Route::get('/', function () {
-    return view('todo.index');
-});
-
+Route::get('/', [TodoController::class, 'index'])->name('todo.index');
 Route::post('/store', [TodoController::class, 'store'])->name('todo.store');
