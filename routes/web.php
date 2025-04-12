e09f96c5fe96651cac370efaab2db3a829bf7ef9
syukriothman10 <syukriothman10@gmail.com>
2025-04-12 16:10:58 +0800

    Add todo.store route
    -for storing new tasks

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
 
 Route::get('/', function () {
     return view('todo.index');
 });
+
+Route::post('/store', [TodoController::class, 'store'])->name('todo.store');
