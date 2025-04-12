7dd5b2101756e82ce0f28bc0ce5ac0d0c8bbc3fc
syukriothman10 <syukriothman10@gmail.com>
2025-04-12 16:21:46 +0800

    Add route to clear session data for todos

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use App\Http\Controllers\TodoController;
 
 Route::get('/', [TodoController::class, 'index'])->name('todo.index');
 Route::post('/store', [TodoController::class, 'store'])->name('todo.store');
+
+Route::post('/clear-session', function () {
+    session()->forget('todos');
+    return response()->noContent();
+});
