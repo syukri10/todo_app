8ccba1d99c66cce244f8c1be98458e5ec07b1609
syukriothman10 <syukriothman10@gmail.com>
2025-04-12 15:48:42 +0800

    Created TodoController and Todo index view
    -Created TodoController to handle task-related logic
    -Created an index Blade view to display the list of existing tasks

---
+++ resources/views/todo/index.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo_App</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            max-width: 600px;
+            margin: 2em auto;
+            background-color: #f9f9f9;
+            color: #333;
+            line-height: 1.6;
+        }
+        h1 {
+            text-align: center;
+            color: #555;
+        }
+        form {
+            display: flex;
+            justify-content: space-between;
+            margin-bottom: 1em;
+            background: #fff;
+            padding: 1em;
+            border-radius: 5px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        }
+    </style>
+    
+</head>
+<body>
+    <h1>My TODO List</h1>
+
+</body>
+</html>
